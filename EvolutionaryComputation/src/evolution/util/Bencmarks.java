e557298b533a437afa7fda38cc016ac103cbe8c5
esgantivar <esgantivar@unal.edu.co>
2015-11-19 12:10:53 -0500

    [doc] cambia ubicacion paquete util

--- EvolutionaryComputation/src/evolution/util/Bencmarks.java
+++ EvolutionaryComputation/src/evolution/util/Bencmarks.java
@@ -1,4 +1,4 @@
-package util;
+package evolution.util;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
--- EvolutionaryComputation/src/evolution/util/Solution.java
+++ EvolutionaryComputation/src/evolution/util/Solution.java
@@ -1,26 +1,27 @@
-package util;
+package evolution.util;
 
-import evolution.Individual;
 import evolution.Population;
+import evolution.individual.Individual;
+
 import java.util.*;
 
 public class Solution {
 
-	public static void sort(Population pop) {
+	public static void sort(Population<?> pop) {
 		Collections.sort(pop.getIndividuals());
 	}
 
-	public static void printStatistics(Population pop) {
+	public static void printStatistics(Population<?> pop) {
 		double best = pop.getIndividual(0).getFitness();
 		double worst = pop.getIndividual(pop.getIndividuals().size()-1).getFitness();
 		double total = 0.0;
-		for (Individual ind : pop.getIndividuals()) {
+		for (Individual<?> ind : pop.getIndividuals()) {
 			total += ind.getFitness();
 		}
 		double mean = total / pop.getIndividuals().size();
 		double median = pop.getIndividual((int) pop.getIndividuals().size() / 2).getFitness();
 		double stdev = 0.0;
-		for (Individual ind : pop.getIndividuals()) {
+		for (Individual<?> ind : pop.getIndividuals()) {
 			stdev += Math.pow(ind.getFitness() - mean, 2);
 		}
 		stdev /= (pop.getIndividuals().size() - 1);
